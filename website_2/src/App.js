ba103192666a11d666b2e7865dddcf98c009f4fb
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-08-02 16:04:54 -0400

    updated index and appjs page and also create different pages to the site/ created light and dark themes

--- website_2/src/App.js
+++ website_2/src/App.js
@@ -2,7 +2,16 @@ import React, { useCallback } from 'react';
 import Particles from "react-particles";
 import { loadFull } from "tsparticles";
 import particlesOptions from "./particles.json";
+import {  ThemeProvider } from 'styled-components';
 import GlobalStyle from './globalStyles';
+import { lightTheme } from './components/Themes';
+import {  Route } from 'react-router';
+import { Routes } from 'react-router-dom';
+
+import Main from './components/Main';
+import AboutPage from './components/AboutPage';
+import MySkillsPage from './components/MySkillsPage';
+import WorkPage from './components/WorkPage';
 
 function App() {
     const particlesInit = useCallback(main => {
@@ -11,7 +20,24 @@ function App() {
 
     return (
         <>
+
+
         <GlobalStyle />
+
+        <ThemeProvider theme={lightTheme}>
+            
+            <Routes>
+                <Route exact path="/" component={Main}/>
+                <Route exact path="/" component={AboutPage}/>
+                <Route exact path="/" component={MySkillsPage}/>
+                <Route exact path="/" component={WorkPage}/>
+
+            </Routes>
+
+        </ThemeProvider>
+
+
+
             <Particles options={particlesOptions} init={particlesInit}/>        
         </>
     );
