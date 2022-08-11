ccef9c671b777857c3dff1540f3cd13838349063
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-08-11 09:38:34 -0400

    new project

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -21,32 +21,31 @@ export const Work = [
     {
         id:3,
         name:"Agency Landing Page",
-        description:"It is build with React, JS and Node.js. Made to be simple, minimal and soft.",
+        description:"It is built with React, JS and Node.js. Made to be simple, minimal and soft.",
         tags:["React","HTML","Nodejs"],
         
         demo:"https://Lululearning.ca/",
         github:"https://github.com/lucaspittam"
+    },{
+        id:4,
+        name:"Budget App | PWA, FullStack ",
+        description:" A pwa that allows users to track their budgets by adding and subtracting expenses and deposits. They can add a transaction with or without a connection.",
+        tags:["Mongodb","Mongoose","PWA","IndexDb"],
+        
+        demo:"https://frozen-tor-46892.herokuapp.com/",
+        github:"https://github.com/lucaspittam/Budget_APP"
+        
     },
     {
-        id:4,
+        id:5,
         name:"Social Network API Backend",
-        description:"",
-        tags:["MongoDB","Mongoose","Nodejs"],
+        description:"API for a social network. Uses a MongoDB database so  the website can handle large amounts of data, Express.js for routing, Mongoose ODM, and the moment package to format time stamps.",
+        tags:["MongoDB","Express.js","Nodejs"],
         
         demo:"https://lucaspittam.com",
         github:"https://github.com/lucaspittam/Socia_Network_API"
        
     },{
-        id:5,
-        name:"",
-        description:"",
-        tags:["react","redux","framer-motion"],
-        
-        demo:"https://lucaspittam.com",
-        github:"https://github.com/lucaspittam"
-        
-    },
-    {
         id:6,
         name:"",
         description:"",
